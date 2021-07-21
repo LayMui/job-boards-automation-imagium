bfbeb81bd98abf323902cfa94f77dfb207e941c7
LayMui <mui_ume@hotmail.com>
2021-07-21 16:52:36 +0800

    Refactor code

--- src/test/java/demo/stepdefinitions/AddJobStepDefinitions.java
+++ src/test/java/demo/stepdefinitions/AddJobStepDefinitions.java
@@ -1,6 +1,9 @@
 package demo.stepdefinitions;
 
 import demo.util.Imagium;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import net.thucydides.core.webdriver.WebDriverFacade;
 import net.serenitybdd.core.Serenity;
 import demo.tasks.Add;
@@ -20,6 +23,8 @@ import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.questions.WebElementQuestion;
 import net.thucydides.core.annotations.Steps;
 
+import org.json.simple.JSONObject;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.slf4j.Logger;
@@ -27,6 +32,8 @@ import org.slf4j.LoggerFactory;
 
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+
+import java.io.IOException;
 
 
 public class AddJobStepDefinitions {
@@ -42,19 +49,49 @@ public class AddJobStepDefinitions {
     @Steps
     NavigationPage navigationPage;
 
-    @Steps
-    Imagium imagium;
 
-    String uid = "";
-    String scrBase64 = "";
+    static String uuid;
+
+    public static String getUID(String testName, String projectKey) {
+        try {
+            RequestSpecification request = RestAssured.given();
+            request.header("content-type", "application/json");
+            JSONObject json = new JSONObject();
+            json.put("TestName", testName);
+            json.put("ProjectKey", projectKey);
+            request.body(json.toJSONString());
+            Response response = request.when().post("http://192.168.1.120:91/api/GetUID");
+            int code = response.getStatusCode();
+            String response_id = response.getBody().asString();
+            System.out.println("TestID: " + response_id);
+            return response_id;
+        } catch (Exception ex) {
+            return ex.toString();
+        }
+    }
+
+    	//Post a request for validation
+	public static void postRequest(String stepName, String uid, String imagebase64) throws IOException {
+		RequestSpecification request1 = RestAssured.given();
+		request1.header("content-type", "application/json");
+		JSONObject jo = new JSONObject();
+		jo.put("TestRunID", uid.replace("\"", ""));
+		jo.put("StepName", stepName);
+		jo.put("ImageBase64", imagebase64);
+		System.out.println("imagebase64:" + imagebase64);
+		request1.body(jo.toJSONString());
+		Response response1 = request1.when().post("http://192.168.1.120:91/api/Validate");
+		String response_id1 = response1.getBody().asString();
+		System.out.println("Response from Imagium: " + response_id1);
+	}
 
     public void takeScreenshot(String testName) {
        // uid = imagium.getUID(testName,  System.getenv("IMAGIUM_API_KEY"));
-        uid = imagium.getUID(testName, "cd827532-cb61-4082-be3a-a331e0822cbd");
+        String scrBase64 = "";
         scrBase64 = ((TakesScreenshot) navigationPage.getDriver()).getScreenshotAs(OutputType.BASE64);
-        
         try {
-            imagium.postRequest(testName, uid, scrBase64);
+            log.info("UUID: " + uuid);
+            Imagium.postRequest(testName, uuid, scrBase64);
         } catch(Exception e) {
             log.info("Exception caught");
         }
@@ -63,7 +100,9 @@ public class AddJobStepDefinitions {
     @Before()
     public void setTheStage() {
         OnStage.setTheStage(new OnlineCast());
-       
+        log.info("Before UUID");
+        uuid = getUID("Job Boards", "cd827532-cb61-4082-be3a-a331e0822cbd");  
+        log.info("UUID: " + uuid);
     }
 
     @After()
@@ -75,6 +114,8 @@ public class AddJobStepDefinitions {
     @Given("^(?:.*) is at the job board$")
     public void jamesIsAtTheJobBoard() {
         theActorCalled("james").attemptsTo(Open.browserOn().the(navigationPage));
+        ((JavascriptExecutor) navigationPage.getDriver()).
+        executeScript("function is_gif_image(i) {\n  return /^(?!data:).*\\.gif/i.test(i.src);\n}\n\nfunction freeze_gif(i) {\n  var c = document.createElement('canvas');\n  var w = c.width = i.width;\n var h = c.height = i.height;\n  c.getContext('2d').drawImage(i, 0, 0, w, h);\n  try {\n    i.src = c.toDataURL(\"image/gif\"); // if possible, retain all css aspects\n  } catch(e) { // cross-domain -- mimic original with all its tag attributes\n    for (var j = 0, a; a = i.attributes[j]; j++)\n      c.setAttribute(a.name, a.value);\n    i.parentNode.replaceChild(c, i);\n  }\n}\n\n\n[].slice.apply(document.images).filter(is_gif_image).map(freeze_gif);");
         takeScreenshot("Jobs Listing");
        
     }
@@ -92,7 +133,9 @@ public class AddJobStepDefinitions {
     
     @Then("^he is able to see the new job added$")
     public void heIsAbleToSeeTheNewJobAdded() {
-        takeScreenshot("New Job Added");
+        ((JavascriptExecutor) navigationPage.getDriver()).
+        executeScript("function is_gif_image(i) {\n  return /^(?!data:).*\\.gif/i.test(i.src);\n}\n\nfunction freeze_gif(i) {\n  var c = document.createElement('canvas');\n  var w = c.width = i.width;\n var h = c.height = i.height;\n  c.getContext('2d').drawImage(i, 0, 0, w, h);\n  try {\n    i.src = c.toDataURL(\"image/gif\"); // if possible, retain all css aspects\n  } catch(e) { // cross-domain -- mimic original with all its tag attributes\n    for (var j = 0, a; a = i.attributes[j]; j++)\n      c.setAttribute(a.name, a.value);\n    i.parentNode.replaceChild(c, i);\n  }\n}\n\n\n[].slice.apply(document.images).filter(is_gif_image).map(freeze_gif);");
+        takeScreenshot("New Job board");
     
     }
 }
